8ceb586d1199be32f777e7eb8b6f4103e71958ef
Scott Ian <sim2487@gmail.com>
2021-03-26 05:51:10 -0700

    Add expanding box and JS to toggle it.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Test');
+const expandButton = document.getElementById('expand');
+const box = document.getElementById('box');
+const collapseButton = document.getElementById('collapse');
+
+expandButton.addEventListener('click', () => {
+	box.classList.add('js-expand');
+	console.log('EXPAND!');
+});
+
+collapseButton.addEventListener('click', () => {
+	box.classList.remove('js-expand');
+});
