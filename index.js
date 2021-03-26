d2a4c6be9e09895d687dc6202ea33ef4d83c0dc1
Scott Ian <sim2487@gmail.com>
2021-03-26 07:30:45 -0700

    Animations for box expand/collapse.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const expandButton = document.getElementById('expand');
 const box = document.getElementById('box');
 const collapseButton = document.getElementById('collapse');
+const landingHeader = document.getElementById('landing-header');
+const detailsContainer = document.getElementById('details-container');
 
 expandButton.addEventListener('click', () => {
 	box.classList.add('js-expand');
-	console.log('EXPAND!');
+	detailsContainer.classList.add('js-fadeToggle');
+	landingHeader.classList.add('js-fadeToggle');
 });
 
 collapseButton.addEventListener('click', () => {
 	box.classList.remove('js-expand');
+	box.classList.add('js-collapse');
+	detailsContainer.classList.remove('js-fadeToggle');
+	landingHeader.classList.remove('js-fadeToggle');
+	setTimeout(() => {
+		box.classList.remove('js-collapse');
+	}, 250);
 });
